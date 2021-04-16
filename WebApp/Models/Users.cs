631f600fa46072ada8a598a079cf8b1d665e9303
Daria Shmielova <dasha.shmyelova@outlook.com>
2021-04-16 16:56:33 +0300

    Models added

---
+++ WebApp/Models/Users.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApp.Models
+{
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("user")]
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ WebApp/Models/UsersLikes.cs
@@ -0,0 +1,21 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApp.Models
+{
+    public class UsersLikes
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("users_book_likes")]
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string UserId { get; set; }
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string BookId { get; set; }
+
+    }
+}
